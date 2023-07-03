e589ba52ae205d5d153ddecbe8cf86166e6621ab
7luky <64417043+7luky@users.noreply.github.com>
2023-07-03 22:43:32 +0900

    #5.1 Tour of CRA

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
 function App() {
   return (
     <div>
-      <h1>Welcome REACT</h1>
+      <h1 className={styles.title}>Welcome REACT</h1>
+      <Button text={"Continue"} />
     </div>
   );
 }
